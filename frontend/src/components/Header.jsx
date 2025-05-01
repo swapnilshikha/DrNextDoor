4e4ee406b1fdeb56ecc8ab868037bdfcfeede27d
swapnilshikha <smegha.3004@gmail.com>
2025-05-01 12:57:11 +0530

    changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,8 @@ import doctorImg from './../assets/doctor.jpeg'
 
 const Header = () => {
   return (
-    <div className="d-flex justify-content-between align-items-center px-5 py-4 rounded-pill bg-dark text-white" style={{ height: '400px' }}>
+    <>
+  <div className="d-flex justify-content-between align-items-center px-5 py-4 rounded-pill bg-dark text-white" style={{ height: '400px' }}>
       <div className="text-start">
         <h2 className="fw-bold">Book Appointment<br />With Trusted Doctors</h2>
         <p className="text-white-50" style={{ fontSize: '20px' }}>
@@ -19,8 +20,8 @@ const Header = () => {
           style={{ maxWidth: '800px', height: 'auto', borderRadius: '150px' }}
         />
       </div>
-
     </div>
+    </>
   );
 };
 
